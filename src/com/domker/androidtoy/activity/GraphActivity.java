5d9f92f6b3c81d1c3d3d9aaad3345526a3283925
maisonwan <maisonwan@gmail.com>
2016-12-20 17:14:34 +0800

    增加Toolbar菜单

--- src/com/domker/androidtoy/activity/GraphActivity.java
+++ src/com/domker/androidtoy/activity/GraphActivity.java
@@ -5,12 +5,19 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.TabLayout.OnTabSelectedListener;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.Toolbar.OnMenuItemClickListener;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.domker.androidtoy.MainActivity;
 import com.domker.androidtoy.R;
+import com.domker.androidtoy.adapter.MenuActionProvider;
 import com.domker.androidtoy.adapter.TabPagerAdapter;
 
 /**
@@ -21,13 +28,13 @@ import com.domker.androidtoy.adapter.TabPagerAdapter;
  * @date 2016年12月7日 下午5:29:55
  */
 public class GraphActivity extends AppCompatActivity implements
-		OnPageChangeListener, OnTabSelectedListener {
+		OnPageChangeListener, OnTabSelectedListener, OnMenuItemClickListener {
 	
 	private ViewPager mViewPager;
 	private TabPagerAdapter mAdapter;
 	private ArrayList<TabLayout.Tab> mTabs;
-	private Toolbar toolbar;
-	private TabLayout tabLayout;
+	private Toolbar mToolbar;
+	private TabLayout mTabLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,38 +50,43 @@ public class GraphActivity extends AppCompatActivity implements
 	}
 
 	private void initActionBar() {
-		toolbar = (Toolbar) this.findViewById(R.id.toolBar);
-		tabLayout = (TabLayout) this.findViewById(R.id.tabLayout);
-		toolbar.setNavigationIcon(R.drawable.app_icon);
-		
-		setSupportActionBar(toolbar);
+		mToolbar = (Toolbar) this.findViewById(R.id.toolBar);
+		mTabLayout = (TabLayout) this.findViewById(R.id.tabLayout);
+		mToolbar.setNavigationIcon(R.drawable.f002);
+		mToolbar.setOnMenuItemClickListener(this);
+		setSupportActionBar(mToolbar);
 	}
 	
 	private void initTab() {
 		// 添加Tabs
 		mTabs = new ArrayList<TabLayout.Tab>();
 		for (String name : TabPagerAdapter.TAB_NAMES) {
-			TabLayout.Tab tab = tabLayout.newTab();
+			TabLayout.Tab tab = mTabLayout.newTab();
 			tab.setText(name);
 			mTabs.add(tab);
-			tabLayout.addTab(tab);
+			mTabLayout.addTab(tab);
 		}
-		tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-		tabLayout.setOnTabSelectedListener(this);
+		mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+		mTabLayout.setOnTabSelectedListener(this);
 	}
 	
-	@SuppressWarnings("deprecation")
 	private void initViewPager() {
 		// 获取ViewPager
 		mViewPager = (ViewPager) findViewById(R.id.viewpager);
 		// 初始化mAdapter
 		mAdapter = new TabPagerAdapter(getSupportFragmentManager());
 		mViewPager.setAdapter(mAdapter);
-		mViewPager.setOnPageChangeListener(this);
+		mViewPager.addOnPageChangeListener(this);
 		// 默认显示第二项
 		mViewPager.setCurrentItem(0);
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.graph_menu, menu);
+		return true;
+	}
+
 	@Override
 	public void onPageScrollStateChanged(int arg0) {
 		
@@ -117,6 +129,30 @@ public class GraphActivity extends AppCompatActivity implements
 	@Override
 	public void onTabUnselected(TabLayout.Tab tab) {
 		
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v7.widget.Toolbar.OnMenuItemClickListener#onMenuItemClick(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onMenuItemClick(MenuItem menuItem) {
+		int id = menuItem.getItemId();
+		String msg = "";
+		switch (id) {
+		case R.id.action_search:
+			msg = "action_search";
+			break;
+		case R.id.action_intent:
+			msg = "action_intent";
+			// 这个地方要注意使用这种方式增加actionprovider不然会报错
+			MenuItemCompat.setActionProvider(menuItem, new MenuActionProvider(this));
+			break;
+		default:
+			break;
+
+		}
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+		return false;
 	}
 
 }
